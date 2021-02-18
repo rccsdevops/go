70b53f4299602ca6361ee62d39e754813c84a73b
rccs <78510768+rccsdevops@users.noreply.github.com>
2021-02-17 17:00:14 -0700

    Update test.go
    removed some extraneous messages

--- repo/test.go
+++ repo/test.go
@@ -1,8 +1,5 @@
-// Package morestrings implements additional functions to manipulate UTF-8
-// encoded strings, beyond what is provided in the standard "strings" package.
 package repo
 
-// ReverseRunes returns its argument string reversed rune-wise left to right.
 func SayHi() string {
 	return string("hello from GitHub")
 }
